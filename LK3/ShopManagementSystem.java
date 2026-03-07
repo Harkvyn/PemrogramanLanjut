1f6108f40a0016fd996a14fbb6f5f5be373403d2
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-07 23:01:42 +0700

    Add ShopManagementSystem class with main method

--- LK3/ShopManagementSystem.java
+++ LK3/ShopManagementSystem.java
@@ -1,1 +1,14 @@
+package LK3;
+import java.util.*;
 
+class ShopManagementSystem {
+  public static void main(String args[]){
+    Product air_putih = new Product();
+    Product potato = new Product();
+    Product Keybourd = new Product();
+    Product Mouse = new Product();
+    Product Baju = new Product();
+    Product Celana = new Product();
+    
+  }
+}
