6eb106da3734fa03d1a452dfc743901d827e14f0
abrahamSamuelsonSiregar-sudo <abrahamsamuelsonsiregar@gmail.com>
2026-03-10 17:54:45 +0700

    Implement Transaction class with basic methods
    Added Transaction class with methods for processing sales and adding items.

--- LK3/Transaction.java
+++ LK3/Transaction.java
@@ -1,3 +1,33 @@
 package LK3;
-import java.util.*;
+//[not finished yet]
+import java.util.ArrayList;
+public class Transaction{
+    private String transactionId;
+    ArrayList<Product> items = new ArrayList<Product>();
+    int totalItems;
+    
+    //constructor
+    public Transaction(String transactionId){
+        this.transactionId = transactionId;
+    }
+    
+    //getter
+    public String getTransactionId(){
+        return transactionId;
+    }
+    public double processSale(){
+        double totalHarga = 0;
+        
+        //proses
+        
+        return totalHarga;
+    }
+    //method overloading
+    public void addItem(Product item){
+        
+    }
+    public void addItem(Product item,int quantity){
+        totalItems += quantity;
+    }
+}
 
