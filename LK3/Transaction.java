a536b436abea0162f28e203485a179f515d14e26
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 13:51:34 +0700

    Fix Transaction class initialization and sale processing
    Updated the Transaction class to initialize totalItems to 0 and modified the processSale method to calculate total price correctly.

--- LK3/Transaction.java
+++ LK3/Transaction.java
@@ -1,10 +1,15 @@
+/**
+ *
+ * @author Abraham
+ */
 package LK3;
-//[not finished yet]
+
+
 import java.util.ArrayList;
 public class Transaction{
     private String transactionId;
-    ArrayList<Product> items = new ArrayList<Product>();
-    int totalItems;
+    ArrayList<Product> items = new ArrayList<>();
+    int totalItems=0;
     
     //constructor
     public Transaction(String transactionId){
@@ -15,19 +20,28 @@ public class Transaction{
     public String getTransactionId(){
         return transactionId;
     }
+    //method
     public double processSale(){
         double totalHarga = 0;
         
         //proses
-        
+        for (Product item : items){
+            //                     harga asli - diskon
+            totalHarga += (item.getPrice() - item.calculateDiscount());
+        }
         return totalHarga;
     }
     //method overloading
     public void addItem(Product item){
-        
+        items.add(item);
+        totalItems+=1;
+        //Mengurangi jumlah stock sebesar 1 (nama setter belum pasti)
+        item.setStockQuantity( item.stockQuantity()-1);
     }
     public void addItem(Product item,int quantity){
+        items.add(item);
         totalItems += quantity;
+        //Mengurangi jumlah stock sebesar int quantity (nama setter belum pasti)
+        item.setStockQuantity( item.stockQuantity() - quantity);
     }
 }
-
