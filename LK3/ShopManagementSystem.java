eafaba61cddf51ce79c5e62804e0cf8c1ad56e0f
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 09:42:31 +0700

    Refactor ShopManagementSystem with product classes

--- LK3/ShopManagementSystem.java
+++ LK3/ShopManagementSystem.java
@@ -1,30 +1,51 @@
 package LK3;
-import java.util.*;
+public class ShopManagementSystem {
 
-class ShopManagementSystem {
-  
-  public static void main(String args[]){
-    Product[] Produk = new Product[6];
-    Produk[0] = new Product("air_putih", 1000);
-    Produk[1] = new Product("Makanan", 3000);
-    Produk[2] = new Product("Mouse", 150000);
-    Produk[3] = new Product("Keybourd", 1000000);
-    Produk[4] = new Product("Baju", 200000);
-    Produk[5] = new Product("Celana", 3000000);
+    public static void main(String[] args) {
+        // disini polymorphismnya, pake array
+        Product[] products = new Product[6];
 
+        products[0] = new FoodProduct("F01", "makan", 10000, 20, "2026-05-01");
+        products[1] = new FoodProduct("F02", "air", 15000, 5, "2026-04-01");
 
+        products[2] = new ElectronicProduct("E01", "Mouse", 200000, 15, "1 Tahun");
+        products[3] = new ElectronicProduct("E02", "Keyboard", 600000, 10, "2 Tahun");
 
+        products[4] = new ClothingProduct("C01", "Kaos", 120000, 30, "L", "Uniqlo");
+        products[5] = new ClothingProduct("C02", "Jaket", 250000, 10, "M", "Nike");
 
-    
+        System.out.println("=== DAFTAR PRODUK ===");
 
-  }
+        for (Product p : products) {
 
-    void TotalPenjualan() {
-    System.out.println("Total Penjualan: ");
-    };
+            p.getProductInfo();
 
-    
-    void Produk_terlaris() {
-    };
+            double discount = p.calculateDiscount();
 
+            System.out.println("Diskon : " + discount);
+            System.out.println("---------------------");
+        }
+        //contoh transaksi pertama
+        Transaction t1 = new Transaction("T01");
+
+        t1.addItem(products[0]);
+        t1.addItem(products[3]);
+
+        double total1 = t1.processSale();
+
+        //cotnh transaksi kedua
+        Transaction t2 = new Transaction("T02");
+
+        t2.addItem(products[4], 2);
+        t2.addItem(products[2]);
+
+        double total2 = t2.processSale();
+
+        double totalSales = total1 + total2;
+
+        System.out.println("=== LAPORAN PENJUALAN ===");
+        System.out.println("Total transaksi 1 : " + total1);
+        System.out.println("Total transaksi 2 : " + total2);
+        System.out.println("Total penjualan   : " + totalSales);
+    }
 }
