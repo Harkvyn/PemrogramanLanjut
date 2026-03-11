f7b088f9471a80c641e4d90244f8dfc2b489a693
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 11:28:43 +0700

    Add ElectronicProduct class with warranty and discount logic

--- LK3/ElectronicProduct.java
+++ LK3/ElectronicProduct.java
@@ -1,1 +1,25 @@
+class ElectronicProduct extends Product {
 
+    private String warrantyPeriod;
+
+    public ElectronicProduct(String id, String name, double price, int stock, String warrantyPeriod) {
+        super(id, name, price, stock);
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    public String getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+
+    @Override
+    public double calculateDiscount() {
+
+        double discount = getPrice() * 0.05;
+
+        if (getPrice() > 500000) {
+            discount += getPrice() * 0.02;
+        }
+
+        return discount;
+    }
+}
