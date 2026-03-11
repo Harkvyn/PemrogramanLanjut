5196df56c751e61d81be05cffad30bdc2c949eac
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 11:28:20 +0700

    Create ClothingProduct class with discount logic
    Added size and brand attributes with corresponding methods. Implemented discount calculation based on size.

--- LK3/ClothingProduct.java
+++ LK3/ClothingProduct.java
@@ -1,1 +1,29 @@
+class ClothingProduct extends Product {
 
+    private String size;
+    private String brand;
+
+    public ClothingProduct(String id, String name, double price, int stock, String size, String brand) {
+        super(id, name, price, stock);
+        this.size = size;
+        this.brand = brand;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public double calculateDiscount() {
+
+        if (size.equals("L") || size.equals("XL")) {
+            return getPrice() * 0.15;
+        }
+
+        return 0;
+    }
+}
