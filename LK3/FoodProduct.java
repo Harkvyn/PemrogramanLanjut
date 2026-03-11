7129b27ad925fe9026987dc9e2e110e2dbe762b3
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 11:29:07 +0700

    Add FoodProduct class with expiry date and discount logic

--- LK3/FoodProduct.java
+++ LK3/FoodProduct.java
@@ -1,1 +1,24 @@
+class FoodProduct extends Product {
 
+    private String expiryDate;
+
+    public FoodProduct(String id, String name, double price, int stock, String expiryDate) {
+        super(id, name, price, stock);
+        this.expiryDate = expiryDate;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    // Override method
+    @Override
+    public double calculateDiscount() {
+
+        if (getStockQuantity() > 10) {
+            return getPrice() * 0.10;
+        }
+
+        return 0;
+    }
+}
