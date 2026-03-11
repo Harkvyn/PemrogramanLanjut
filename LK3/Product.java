9aedd14a4ecc0e0f054946b90c203f9146bcddf7
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 11:33:01 +0700

    Refactor Product class to be abstract with new fields

--- LK3/Product.java
+++ LK3/Product.java
@@ -1,12 +1,61 @@
 package LK3;
 import java.util.*;
 
-class Product {
-    String nama;
-    int harga;
+abstract class Product {
 
-    Product(String nama, int harga) {
-        this.nama = nama;
-        this.harga = harga;
+    private String productId;
+    private String name;
+    private double price;
+    private int stockQuantity;
+    
+    public Product() {
+
+    }
+
+    public Product(String productId, String name, double price, int stockQuantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    
+    public abstract double calculateDiscount();
+    
+    public void getProductInfo() {
+        System.out.println("ID Produk : " + productId);
+        System.out.println("Nama      : " + name);
+        System.out.println("Harga     : " + price);
+        System.out.println("Stok      : " + stockQuantity);
+    }
+
+    // Overloading di method ini
+    public void updateStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    public void updateStock(int quantity, String reason) {
+        stockQuantity += quantity;
+        System.out.println("Update stok karena: " + reason);
     }
 }
