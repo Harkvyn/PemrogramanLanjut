7c7deac766f8f85dc509ee9b052f27df25c521a0
Pecel Antartika <hamamyusufa@gmail.com>
2026-03-11 13:50:38 +0700

    Update FoodProduct.java

--- LK3/FoodProduct.java
+++ LK3/FoodProduct.java
@@ -1,24 +1,30 @@
-class FoodProduct extends Product {
+/**
+ *
+ * @author Ridho & Mubin
+ */
+
+package LK3;
+
+public class FoodProduct extends Product {
 
     private String expiryDate;
 
-    public FoodProduct(String id, String name, double price, int stock, String expiryDate) {
-        super(id, name, price, stock);
+    public FoodProduct(String productId, String name, double price, int stockQuantity, String expiryDate) {
+        super(productId, name, price, stockQuantity);
         this.expiryDate = expiryDate;
     }
 
-    public String getExpiryDate() {
-        return expiryDate;
-    }
-
-    // Override method
     @Override
     public double calculateDiscount() {
-
-        if (getStockQuantity() > 10) {
+        if (stockQuantity() > 10) {
             return getPrice() * 0.10;
         }
-
         return 0;
+    }
+
+    @Override
+    public void getProductInfo() {
+        super.getProductInfo();
+        System.out.println("Expiry Date : " + expiryDate);
     }
 }
